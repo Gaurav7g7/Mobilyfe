d8f155a83bc90dea8ba36a933ef3230048ab44e5
sibange <simon@LAPTOP-HT4E4ADI.localdomain>
2025-02-15 22:29:04 -0600

    Addition of additional categories

--- map.py
+++ map.py
@@ -12,8 +12,8 @@ def get_locations(lat:float, lon: float, dist: int = 1000, location_type: str = 
     :param lat: Latitude of the point
     :param lon: Longitude of the point
     :param dist: (optional) Maximum distance of the locations from the selected point in m. Default: 1000
-    :param location_type: (optional) Type of the location that is returned.#
-        Currently supported: restaurant (default), park
+    :param location_type: (optional) Type of the location that is returned.
+        Currently supported: restaurant (default), park, sports, socialize
     :return: List of Dictionaries which contain three entries for each location:
                                 - name: Name of the location
                                 - lon: Latitude
@@ -22,9 +22,15 @@ def get_locations(lat:float, lon: float, dist: int = 1000, location_type: str = 
     api = overpy.Overpass()
 
     if location_type == 'restaurant':
-        tags = '[amenity=restaurant]'
+        tags = '["amenity"~"restaurant|fast_food"]'
     elif location_type == 'park':
-        tags = '[leisure=park]'
+        tags = '["leisure"~"park|garden|nature_reserve"]'
+    elif location_type == 'sports':
+        tags = ('["leisure"~"disc_golf_course|bowling_alley|disc_golf_course|fitness_centre|golf_course|ice_rink|'
+                'sports_centre|sports_hall|miniature_golf|pitch|stadium|track|swimming_pool"]')
+    elif location_type == 'socialize':
+        tags = '["amenity"~"bar|biergarten|cafe|pub"]'
+
     else:
         print('Location type not supported. Supported types: restaurant, park')
         return []
@@ -104,7 +110,7 @@ def mapping_call(lat: float, lon: float, radius: int = 1000, location_type: str 
     :param lon: Longitude of the point
     :param radius: Maximum distance of the locations from the selected point in m. Default: 1000
     :param location_type: (optional) Type of the location that is returned.#
-        Currently supported: restaurant (default), park
+        Currently supported: restaurant (default), park, sports, socialize
     :param mobility_mode: (optional) Can be used to select the current vehicle the user has.
         When not given computes values for walking, cycle and car.
         Possible values: foot, cycle, car,
